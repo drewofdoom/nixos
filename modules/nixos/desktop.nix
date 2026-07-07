56d073a4eea6aa4cde2e49bbe3a7ceb50ddc9180
Drew DeVore <drew@devorcula.com>
2026-07-06 23:06:17 -0600

    add ssh

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -35,4 +35,17 @@
     yabridgectl
     winetricks
   ];
+
+  services.openssh = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = [ "drew" ];
+        MaxAuthTries = 3;
+        PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+      };
+    };
 }
