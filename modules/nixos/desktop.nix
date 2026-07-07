65b1cd43f42451cb457b34d2d576cc6b3633381f
drewofdoom <drew@devorcula.com>
2026-07-06 23:16:10 -0600

    update ssh settings

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -40,10 +40,7 @@
       enable = true;
       openFirewall = true;
       settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
-        AllowUsers = [ "drew" ];
         MaxAuthTries = 3;
         PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
       };
