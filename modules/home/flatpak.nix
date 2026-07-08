7486b27fdbc196db723ce71c4afe86a87a0e885f
Drew DeVore <drew@devorcula.com>
2026-07-08 14:47:20 -0600

    add some flatpaks

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
 
+  services.flatpak = {
     remotes = [{
       name = "flathub";
       location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
@@ -7,6 +8,16 @@
 
     packages = [
       "de.haeckerfelix.Fragments"
+      "io.github.flattool.Warehouse"
+      "com.github.tchx84.Flatseal"
+      rec {
+        appId = "com.github.magillos.cable";
+        sha256 = "sha256:fb754c5f9dd296d650bdaf9a9915f627e5748205944a22ed1586c516b56af549";
+        bundle = "${pkgs.fetchurl {
+          url = "https://github.com/magillos/Cable/releases/download/0.10.12/Cable-0.10.12.flatpak";
+          inherit sha256;
+        }}";
+      }
     ];
 
     # Cleans up flatpaks not declared here
