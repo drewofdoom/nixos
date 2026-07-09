d6059c638f51641d99de958ac454d0c5dc3feaa6
Drew DeVore <drew@devorcula.com>
2026-07-08 20:02:49 -0600

    add allowed rates for pipewire

--- hosts/blackstar/pipewire.nix
+++ hosts/blackstar/pipewire.nix
@@ -16,6 +16,7 @@
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
+      "default.clock.allowed-rates" = [ 44100 48000 88200 96000 192000 ];
       "default.clock.quantum" = 64;
       "default.clock.min-quantum" = 64;
       "default.clock.max-quantum" = 64;
