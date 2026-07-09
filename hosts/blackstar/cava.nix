b0d05159a89d9cb2ec6ec76b8f18d234eb755007
Drew DeVore <drew@devorcula.com>
2026-07-08 19:56:40 -0600

    add some more cava options

--- hosts/blackstar/cava.nix
+++ hosts/blackstar/cava.nix
@@ -1,7 +1,11 @@
 {local, username, ...}: {
   home-manager.users.${username}.programs.cava.settings = {
     general.framerate = 60;
-    smoothing.noise_reduction = 88;
     live-config = 1;
+    blend_direction = "up";
+    smoothing = {
+      noise_reduction = 25;
+      monstercat = 0;
+    };
   };
 }
